92aa814667d4f8c6ff1f219e74084200fe5c8f03
Chaiwat Klanrueangsaeng <6310682742@cn332>
2023-03-09 12:14:15 +0700

    link tracking video

--- cartrackProject/tasks/views.py
+++ cartrackProject/tasks/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from .tasks import Detect, Opt
+from .models import *
 
 def testDetect(request):
     opt = Opt()
-    opt.source = "./videos_uploaded/videoKodShort.mp4"
+    source_path = "videos_uploaded/"
+    source_name = "VideoKodShort.mp4"
+    target_path = "videos_tracking/object_tracking/"
+    opt.source = f"{source_path}{source_name}"
     d = Detect(opt)
+    my_files = OriginalVideo.objects.filter(video__contains=f"{source_path}{source_name}")
+    TrackingVideo.objects.create(
+        original = my_files.first(),
+        video = f"{target_path}{source_name}",
+    )
     return render(request, 'tasks/testDetect.html', {
         'd': d,
     })
