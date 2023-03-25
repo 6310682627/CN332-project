6af37da092ad0ae3fc062f6580182002b3241f6f
6310682593 <6310682593@student.tu.ac.th>
2023-03-25 23:50:20 +0700

    show video & path

--- cartrackProject/cartrackProject/urls.py
+++ cartrackProject/cartrackProject/urls.py
@@ -19,10 +19,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
+    path('users/', include('users.urls')),
     path('admin/', admin.site.urls),
     path('', include('tasks.urls')),
-    path('users/', include('users.urls')),
-]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
