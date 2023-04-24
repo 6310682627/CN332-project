ccb4ce23c638495ef2d80df34bfa1666ce14503b
Onjira Wongpolganan <6310611022@cn103>
2023-04-24 14:38:25 +0700

    edit templates

--- cartrackProject/tasks/forms.py
+++ cartrackProject/tasks/forms.py
@@ -30,17 +30,17 @@ class TaskForm(forms.ModelForm):
 
 class LoopForm(forms.ModelForm):
 
-    loop_id = forms.IntegerField(validators=[MinValueValidator(0)])
-    x1 = forms.DecimalField(validators=[MinValueValidator(0)])
-    y1 = forms.DecimalField(validators=[MinValueValidator(0)])
-    x2 = forms.DecimalField(validators=[MinValueValidator(0)])
-    y2 = forms.DecimalField(validators=[MinValueValidator(0)])
-    x3 = forms.DecimalField(validators=[MinValueValidator(0)])
-    y3 = forms.DecimalField(validators=[MinValueValidator(0)])
-    x4 = forms.DecimalField(validators=[MinValueValidator(0)])
-    y4 = forms.DecimalField(validators=[MinValueValidator(0)])
-    summary_location_x = forms.DecimalField(validators=[MinValueValidator(0)])
-    summary_location_y = forms.DecimalField(validators=[MinValueValidator(0)])
+    loop_id = forms.IntegerField(validators=[MinValueValidator(0)],widget=forms.TextInput(attrs={'class': 'form-control'}))
+    x1 = forms.DecimalField(validators=[MinValueValidator(0)],widget=forms.TextInput(attrs={'class': 'form-control'}))
+    y1 = forms.DecimalField(validators=[MinValueValidator(0)],widget=forms.TextInput(attrs={'class': 'form-control'}))
+    x2 = forms.DecimalField(validators=[MinValueValidator(0)],widget=forms.TextInput(attrs={'class': 'form-control'}))
+    y2 = forms.DecimalField(validators=[MinValueValidator(0)],widget=forms.TextInput(attrs={'class': 'form-control'}))
+    x3 = forms.DecimalField(validators=[MinValueValidator(0)],widget=forms.TextInput(attrs={'class': 'form-control'}))
+    y3 = forms.DecimalField(validators=[MinValueValidator(0)],widget=forms.TextInput(attrs={'class': 'form-control'}))
+    x4 = forms.DecimalField(validators=[MinValueValidator(0)],widget=forms.TextInput(attrs={'class': 'form-control'}))
+    y4 = forms.DecimalField(validators=[MinValueValidator(0)],widget=forms.TextInput(attrs={'class': 'form-control'}))
+    summary_location_x = forms.DecimalField(validators=[MinValueValidator(0)],widget=forms.TextInput(attrs={'class': 'form-control'}))
+    summary_location_y = forms.DecimalField(validators=[MinValueValidator(0)],widget=forms.TextInput(attrs={'class': 'form-control'}))
 
 
     class Meta:
@@ -48,3 +48,4 @@ class LoopForm(forms.ModelForm):
         fields = ['loop_name', 'loop_id', 'orientation',
                   'x1', 'y1', 'x2', 'y2', 'x3', 'y3', 'x4', 'y4',
                   'summary_location_x', 'summary_location_y']
+        widgets = {'loop_name' : forms.TextInput(attrs={'class': 'form-control'}), 'orientation' : forms.TextInput(attrs={'class': 'form-control'})}
